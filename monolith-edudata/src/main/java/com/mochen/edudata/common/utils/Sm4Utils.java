32f335bea51233b2a70d32228757c95c5e5e761e
DESKTOP-DMIG9HS\姚广举 <yao_guangju@163.com>
2022-03-15 14:35:06 +0800

    学情大数据项目-对称加密-国密

---
+++ monolith-edudata/src/main/java/com/mochen/edudata/common/utils/Sm4Utils.java
@@ -0,0 +1,24 @@
+package com.mochen.edudata.common.utils;
+
+import cn.hutool.core.util.CharsetUtil;
+import cn.hutool.crypto.SmUtil;
+import cn.hutool.crypto.symmetric.SymmetricCrypto;
+import com.mochen.edudata.common.config.CustomizeConfig;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Component
+public class Sm4Utils {
+
+    @Resource
+    private CustomizeConfig customizeConfig;
+
+    public String getDecryptStr(String stringHex){
+        String key = customizeConfig.getSm4key();
+        SymmetricCrypto sm4 = SmUtil.sm4(key.getBytes());
+        return sm4.decryptStr(stringHex, CharsetUtil.CHARSET_UTF_8);
+
+    }
+
+}
