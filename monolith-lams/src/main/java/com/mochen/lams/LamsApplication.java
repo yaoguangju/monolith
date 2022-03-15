71aea7d3f2eee0e47d747d459076f844535bc529
DESKTOP-DMIG9HS\姚广举 <yao_guangju@163.com>
2022-03-15 15:05:31 +0800

    教学序列化系统-项目初始化

---
+++ monolith-lams/src/main/java/com/mochen/lams/LamsApplication.java
@@ -0,0 +1,12 @@
+package com.mochen.lams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class LamsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LamsApplication.class, args);
+    }
+}
