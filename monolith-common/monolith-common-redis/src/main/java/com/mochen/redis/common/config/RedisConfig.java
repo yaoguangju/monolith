79e39b639b1f68df988f6106ccc26a3c43d885af
DESKTOP-DMIG9HS\姚广举 <yao_guangju@163.com>
2022-03-19 11:29:34 +0800

    RedisConfig序列化问题

--- monolith-common/monolith-common-redis/src/main/java/com/mochen/redis/common/config/RedisConfig.java
+++ monolith-common/monolith-common-redis/src/main/java/com/mochen/redis/common/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.mochen.redis.common.config;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -47,4 +48,26 @@ public class RedisConfig extends CachingConfigurerSupport
         redisTemplate.afterPropertiesSet();
         return redisTemplate;
     }
+
+//    @Bean
+//    public RedisTemplate<Object,Object> redisTemplate(RedisConnectionFactory factory){
+//        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+//        redisTemplate.setConnectionFactory(factory);
+//        // 使用Jackson2JsonRedisSerialize 替换默认序列化
+//        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        //忽略任何值为null的属性
+//        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+//        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+//        // 设置key和value的序列化规则
+//        redisTemplate.setKeySerializer(new StringRedisSerializer());
+//        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+//        // 设置hashKey和hashValue的序列化规则
+//        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+//        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+//        //afterPropertiesSet和init-method之间的执行顺序是afterPropertiesSet 先执行，init-method 后执行。
+//        redisTemplate.afterPropertiesSet();
+//        return redisTemplate;
+//
+//    }
 }
