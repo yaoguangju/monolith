b90e99b82985af42230c1015bdbfc2534279b333
yao_guangju@163.com <ygj125159>
2022-04-05 04:35:13 +0800

    canal样例完成

---
+++ monolith-complex/monolith-complex-canal/src/test/java/com/mochen/canal/CanalTests.java
@@ -0,0 +1,13 @@
+package com.mochen.canal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CanalTests {
+
+    @Test
+    public void test(){
+
+    }
+}
