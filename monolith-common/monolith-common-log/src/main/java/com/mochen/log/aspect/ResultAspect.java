efa85143d083024d36a2b926141e1a69483203bd
DESKTOP-DMIG9HS\姚广举 <yao_guangju@163.com>
2022-04-11 18:12:59 +0800

    日志公共组件样例收集

--- monolith-common/monolith-common-log/src/main/java/com/mochen/log/aspect/ResultAspect.java
+++ monolith-common/monolith-common-log/src/main/java/com/mochen/log/aspect/ResultAspect.java
@@ -1,6 +1,5 @@
-package com.mochen.core.aop;
+package com.mochen.log.aspect;
 
-import com.mochen.core.exception.ControllerExceptionAdvice;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -12,7 +11,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 
