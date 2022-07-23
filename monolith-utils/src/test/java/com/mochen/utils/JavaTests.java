a7d603ca2a5318f6daecf2b829bed92f88bb6cc7
姚广举#A04787 <guangju.yao@casstime.com>
2022-07-23 14:46:29 +0800

    删除敏感标识

--- monolith-utils/src/test/java/com/mochen/utils/JavaTests.java
+++ monolith-utils/src/test/java/com/mochen/utils/JavaTests.java
@@ -1,16 +1,12 @@
 package com.mochen.utils;
 
-import cn.hutool.core.text.CharSequenceUtil;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.text.MessageFormat;
 
-/**
- * @author 姚广举
- * @version 创建时间：
- * @mail 邮箱: guangju.yao@casstime.com
- */
+
 @SpringBootTest
 public class JavaTests {
 
